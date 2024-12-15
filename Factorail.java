ad9ffbb1d7c0b08480b3256ebb6eb91d5c15b40c
imshari29 <160562050+imshari29@users.noreply.github.com>
2024-12-15 12:39:31 +0530

    Add files via upload

--- Factorail.java
+++ Factorail.java
@@ -3,13 +3,22 @@ import java.util.Scanner;
 public class Factorail {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
-        System.out.print("Enter the value of n :");
+        System.out.print("Enter the value of n : ");
         int n = in.nextInt();
         int fact = 1;
+        int sum = 0;
         for (int i = 1; i <= n; i++) {
             fact = fact * i;
         }
         System.out.println(fact);
+        while (fact > 0){
+            int d = fact % 10;
+            if(d == 0){
+                sum++;
+            }
+            fact = fact / 10;
+        }
+        System.out.println("The trailing zeros in fact of "+n+" is : "+sum);
     }
 }
 // 1 * 2 * 3 * 4 * 5
